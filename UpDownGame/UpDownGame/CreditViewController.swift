286f8c2cc490b0da9287df84769fb6ad3290147b
crystal <crystal.h@kakaovx.com>
2022-12-11 01:52:41 +0900

    UpDownGame 만든 호엥이~!

---
+++ UpDownGame/UpDownGame/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by crystal on 2022/12/11.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+
+}
